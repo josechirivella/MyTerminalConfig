4de24bac0f4a1f70b5c23e9c41efcec1b0a7bd82
Jose Chirivella <jchirivella@foodlogiq.com>
2018-10-01 11:37:46 -0400

    Removed cisco config

--- Projects/config/cisco.sh
+++
@@ -1,9 +0,0 @@
-# Cisco aliases and PATH
-
-# Phabricator
-export PATH="$HOME/cisco/tools/arcanist/bin:$PATH"
-
-# Aliases
-alias vimcisco="vim $HOME/MyTerminalConfig/Projects/config/cisco.sh"
-alias compose="$HOME/cisco/compose"
-alias web="$HOME/cisco/compose/web_ui/"
