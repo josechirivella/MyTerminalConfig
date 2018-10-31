b288d86f735b31048a8b310f342a8680d4ccaf47
Jose Chirivella <jrchg1403@gmail.com>
2018-10-31 16:32:41 -0400

    Added git file for setting globals

---
+++ git.sh
@@ -0,0 +1,9 @@
+git_install()
+{
+	read -p "Enter the email that you want to set for Git and press [ENTER]: " email
+	echo 'Setting git global values'
+	git config --global user.name "Jose Chirivella"
+	git config --global user.email "$email" 
+}
+
+git_install
