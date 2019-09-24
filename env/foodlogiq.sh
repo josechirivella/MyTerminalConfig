0af08a269bbab137a9ca0fcadd70b34553532248
Jose Chirivella <jrchg1403@gmail.com>
2019-09-24 14:42:09 -0400

    Added firefox_bin for foodlogiq env file

--- env/foodlogiq.sh
+++ env/foodlogiq.sh
@@ -4,3 +4,4 @@ alias flcode="cd ~/foodlogiq/src/github.com/FoodLogiQ/"
 # Go Config
 export GOPATH=$HOME/foodlogiq
 export PATH=$PATH:$GOPATH/bin
+export FIREFOX_BIN="/Applications/Firefox Developer Edition.app/Contents/MacOS/firefox-bin"
