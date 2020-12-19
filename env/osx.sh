29b613a1c9651a3362d6c0651797b34570545395
Jose Chirivella <jrchg1403@gmail.com>
2020-12-19 13:12:24 -0500

    Added an alias for fixing the app store 'cancelled'

--- env/osx.sh
+++ env/osx.sh
@@ -1,3 +1,4 @@
 export FIREFOX_BIN="/Applications/Firefox Developer Edition.app/Contents/MacOS/firefox-bin"
 export PATH="$PATH:/usr/local/opt/mongodb-community@3.6/bin:/usr/local/sbin"
 export LDFLAGS="-L/usr/local/opt/llvm/lib -Wl,-rpath,/usr/local/opt/llvm/lib"
+alias fixappstore="rm -r '$TMPDIR/../C/com.apple.appstore/'* && killall -9 appstoreagent"
