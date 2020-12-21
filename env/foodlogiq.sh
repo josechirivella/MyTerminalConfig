0e70964b633413fdb826eb8f222bb53d99c0fec8
Jose Chirivella <jchirivella@foodlogiq.com>
2020-12-21 17:48:35 -0500

    Added ionic cordova alias

--- env/foodlogiq.sh
+++ env/foodlogiq.sh
@@ -1,6 +1,7 @@
 # FoodLogic aliases
 alias fl="cd ~/foodlogiq/"
 alias flcode="cd ~/foodlogiq/src/github.com/FoodLogiQ/"
+alias ic="ionic cordova"
 # Go Config
 export GOPATH=$HOME/foodlogiq
 export PATH=$PATH:$GOPATH/bin
