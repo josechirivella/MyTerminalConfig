8b543d4f544a5c5155fa645ac15aa9808c96681c
Jose Chirivella <jrchg1403@gmail.com>
2021-09-29 22:46:37 -0400

    added yarn bin

--- env/wsl.sh
+++ env/wsl.sh
@@ -1,2 +1,2 @@
-export PATH="$PATH:/usr/local/go/bin:$GOPATH/bin"
+export PATH="$PATH:/usr/local/go/bin:$GOPATH/bin:$HOME/.yarn/bin"
 export CHROME_BIN=/mnt/c/Program\ Files/BraveSoftware/Brave-Browser/Application/brave.exe
