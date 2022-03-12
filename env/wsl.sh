795abb209478479917c5d7ba05cea4b79acb2dee
Jose Chirivella <jrchg1403@gmail.com>
2022-03-12 01:14:34 -0500

    Added rbenv and yarn path

--- env/wsl.sh
+++ env/wsl.sh
@@ -1,2 +1,2 @@
-export PATH="$PATH:/usr/local/go/bin:$GOPATH/bin:$HOME/.yarn/bin"
+export PATH="$PATH:/usr/local/go/bin:$GOPATH/bin:$HOME/.yarn/bin:$HOME/.rbenv/bin"
 export CHROME_BIN=/mnt/c/Program\ Files/BraveSoftware/Brave-Browser/Application/brave.exe
