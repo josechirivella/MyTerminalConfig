7f410aca828b74e8368da708e435b81ad6bf5c31
Jose Chirivella <jrchg1403@gmail.com>
2022-10-24 00:17:43 -0500

    Added function to start services

--- env/wsl.sh
+++ env/wsl.sh
@@ -1,2 +1,9 @@
 export PATH="$PATH:/usr/local/go/bin:$GOPATH/bin:$HOME/.yarn/bin:$HOME/.rbenv/bin"
 export CHROME_BIN=/mnt/c/Program\ Files/BraveSoftware/Brave-Browser/Application/brave.exe
+
+function startServices() {
+  services=("mysql" "redis-server", "nginx")
+  for service in "${services[@]}"; do
+    sudo service $service start
+  done
+}
