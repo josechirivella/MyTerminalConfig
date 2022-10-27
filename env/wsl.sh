7c38d054e87a1a78203aafb56e38a6eb5eccaa49
Jose Chirivella <jrchg1403@gmail.com>
2022-10-27 12:52:11 -0500

    Removed a typo in the services array

--- env/wsl.sh
+++ env/wsl.sh
@@ -2,7 +2,7 @@ export PATH="$PATH:/usr/local/go/bin:$GOPATH/bin:$HOME/.yarn/bin:$HOME/.rbenv/bi
 export CHROME_BIN=/mnt/c/Program\ Files/BraveSoftware/Brave-Browser/Application/brave.exe
 
 function startServices() {
-  services=("mysql" "redis-server", "nginx")
+  services=("mysql" "redis-server" "nginx")
   for service in "${services[@]}"; do
     sudo service $service start
   done
