742aa2e326de9d4b5c62fad7f4fdc4fc62f7a279
Jose Chirivella <jrchg1403@gmail.com>
2023-01-03 12:15:57 -0600

    Added a function to convert images in the current directory to webp

--- env/env.sh
+++ env/env.sh
@@ -57,6 +57,15 @@ function clonefrustrated() {
   git clone git@github.com:frustrateddeveloper/$1
 }
 
+function convertImagesToWebp() {
+  echo 'Converting all .png, .jpg, and .jpeg files to .webp...'
+  for f in $(find . -name '*.png' -or -name '*.jpg' -or -name '*.jpeg'); do
+    if [ ! -f "${f%.*}.webp" ]; then
+      cwebp -q 60 $f -o ${f%.*}.webp
+    fi
+  done
+}
+
 #mkdir and cd
 function mkcd() { mkdir -p "$@" && cd "$_"; }
 
